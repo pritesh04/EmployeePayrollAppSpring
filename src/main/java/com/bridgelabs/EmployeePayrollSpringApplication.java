06e5f5e72a3d6afd9090ede640897b58ea46a033
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-09-30 21:34:41 +0530

    [Pritesh] Used lombok Library for logging

--- src/main/java/com/bridgelabs/EmployeePayrollSpringApplication.java
+++ src/main/java/com/bridgelabs/EmployeePayrollSpringApplication.java
@@ -3,11 +3,15 @@ package com.bridgelabs;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import lombok.extern.slf4j.Slf4j;
+
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollSpringApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+		log.error("Error generated ");
 	}
 
 }
