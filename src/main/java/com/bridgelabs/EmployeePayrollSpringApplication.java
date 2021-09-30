a37f057aec127274772b7a3f008f6b1f202e5f64
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-09-30 21:41:03 +0530

    [Pritesh] Added Different levels of logging

--- src/main/java/com/bridgelabs/EmployeePayrollSpringApplication.java
+++ src/main/java/com/bridgelabs/EmployeePayrollSpringApplication.java
@@ -2,6 +2,7 @@ package com.bridgelabs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -10,8 +11,8 @@ import lombok.extern.slf4j.Slf4j;
 public class EmployeePayrollSpringApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EmployeePayrollSpringApplication.class, args);
-		log.error("Error generated ");
+		ApplicationContext context=  SpringApplication.run(EmployeePayrollSpringApplication.class, args);
+		log.info("AddressBookApp started in {} Envoronment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
