a8a36c28f8f4b460f37185d44e3a2d22420b37ec
MSDOMAIN1\pbhanga <priteshraj.bhangale@morningstar.com>
2021-10-01 09:37:53 +0530

    [Pritesh] Added remaining properties to EmployeepayrollDTo

--- src/main/java/com/bridgelabs/dto/EmployeePayrollDto.java
+++ src/main/java/com/bridgelabs/dto/EmployeePayrollDto.java
@@ -1,16 +1,32 @@
 package com.bridgelabs.dto;
 
+import java.util.List;
+
+import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
 
 import lombok.Data;
+import lombok.ToString;
 
 @Data
-public class EmployeePayrollDto {
-	@Pattern(regexp="^[A-Z]{1}[a-zA-Z\\s]{2,}$",message="Employee name Invalid")
+public @ToString class EmployeePayrollDto {
+
+	@Pattern(regexp="^[A-Z]{1}{a-zA-Z\\s}{2,}$",message="Employee name is invalid")
 	public String name;
-	
+	@Min(value=500,message="Min wage should above 500")
 	public long salary;
+	
+	public String gender;
+	
+	public String startDate;
+	
+	public String note;
+	
+	public String profilePic;
+	
+	public List<String> department;
+
 
 	public EmployeePayrollDto(String name, long salary) {
 		super();
